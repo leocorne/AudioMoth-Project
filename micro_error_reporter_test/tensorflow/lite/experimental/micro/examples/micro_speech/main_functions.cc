2f3405490a1c52a8e162e009ce08cfd8e46dc0a6
leocorne <leonardocornelatti@gmail.com>
2020-06-16 16:18:29 +0200

    Reduced binary
    - Removed unused files
    - Changed fat32 file system

--- micro_error_reporter_test/tensorflow/lite/experimental/micro/examples/micro_speech/main_functions.cc
+++ micro_error_reporter_test/tensorflow/lite/experimental/micro/examples/micro_speech/main_functions.cc
@@ -30,6 +30,7 @@ extern "C" int mainfun(int argc, char* argv[]) {
   tflite::MicroErrorReporter micro_error_reporter;
   tflite::ErrorReporter* error_reporter = &micro_error_reporter;
 
+  error_reporter->Report("Setting up...");
   // Map the model into a usable data structure. This doesn't involve any
   // copying or parsing, it's a very lightweight operation.
   const tflite::Model* model = ::tflite::GetModel(g_tiny_conv_model_data);
@@ -40,6 +41,8 @@ extern "C" int mainfun(int argc, char* argv[]) {
         model->version(), TFLITE_SCHEMA_VERSION);
     return 1;
   }
+
+  error_reporter->Report("Version OK");
 
   // This pulls in all the operation implementations we need.
   tflite::ops::micro::AllOpsResolver resolver;
@@ -52,9 +55,12 @@ extern "C" int mainfun(int argc, char* argv[]) {
   tflite::SimpleTensorAllocator tensor_allocator(tensor_arena,
                                                  tensor_arena_size);
 
+  error_reporter->Report("Created tensor_allocator");
   // Build an interpreter to run the model with.
   tflite::MicroInterpreter interpreter(model, resolver, &tensor_allocator,
                                        error_reporter);
+
+  error_reporter->Report("Created interpreter");
 
   // Get information about the memory area to use for the model's input.
   TfLiteTensor* model_input = interpreter.input(0);
@@ -73,8 +79,11 @@ extern "C" int mainfun(int argc, char* argv[]) {
 
   RecognizeCommands recognizer(error_reporter);
 
+  error_reporter->Report("Created provider and recognizer");
   int32_t previous_time = 0;
   // Keep reading and analysing audio data in an infinite loop.
+
+  error_reporter->Report("Successful setup");
   while (true) {
     // Fetch the spectrogram for the current time.
     const int32_t current_time = LatestAudioTimestamp();

7c47319b431e7119ad6d3478db9f58a7a18633dc
leocorne <leonardocornelatti@gmail.com>
2020-09-08 09:32:09 +0100

    Cleanup
    Expanded readme and added some comments

--- README.md
+++ README.md
@@ -22,3 +22,13 @@ The software works similarly to the [standard AudioMoth firmware](https://github
 Copyright 2017 [Open Acoustic Devices](http://www.openacousticdevices.info/).
 
 [MIT license](http://www.openacousticdevices.info/license).
+
+
+### Code ###
+Most of the folders here are simply embedded libraries we are using.
+
+The src folder has the AudioMoth-specific code, including the main recording loop and several AudioMoth functions.
+
+The ei_library folder has all of the Edge Impulse code. Within this, the model-parameters folder has the parameters for our specific EI model. The tflite-model folder holds the weights of the TensorFlow Lite neural network. The source folder has the source code for the code developed to interact with the EI SDK. The edge-impulse-sdk folder has various libraries needed to run our model, such as DSP libraries, CMSIS code and the TFLite source code. The porting_audiomoth folder within this has AudioMoth-specific implementations of some generic EI methods, such as logging and getting the time. 
+
+The GNU ARM v4.9.3 - Release folder contains the binary files that allow us to deploy the code to AudioMoth. Specifically, AudioMoth-Project.bin can be deployed on AudioMoth through the AudioMoth Flash App. This pre-generated binary is running Conv1D_S on the coquí dataset, so it will save coquí recordings if it hears any (to test this, you can find several coquí recordings on YouTube or freesound.org)
--- ei_library/source/ei_main_loop.cpp
+++ ei_library/source/ei_main_loop.cpp
@@ -18,10 +18,12 @@
 float prob_array[RECORDING_LENGTH_IN_BUFFERS];
 int prob_index;
 
+// Create probability array for log file
 extern "C" void makeProbArray(){
     prob_index = 0;
 }
 
+// Print probability array for log file
 extern "C" void printProbArray(){
     ei_printf_force("[");
     for (size_t ix = 0; ix < prob_index; ix++) {
@@ -32,14 +34,15 @@ extern "C" void printProbArray(){
     ei_printf_force("]\n");
 }
 
+// Return pointer to probability array
 extern "C" float * getProbArray(){
     return prob_array;
 }
 
+// Main function to classify audio signal
 extern "C" float ei_classify(int16_t* raw_features, int raw_features_size, int signal_size, float* signal_start_address){
 
     // Convert raw inputs to float as this is what the Edge Impulse model takes in.
-    // TODO: Edit EI code so it will take int16_t 
 
     float *float_features = signal_start_address;
 
@@ -47,34 +50,12 @@ extern "C" float ei_classify(int16_t* raw_features, int raw_features_size, int s
         float_features[i] = (float)raw_features[i];
     }
 
-    //ei_printf_force("Generated float_features \n");
-
     ei_impulse_result_t result;
     signal_t signal;
     
     numpy::signal_from_buffer(&float_features[0], signal_size, &signal);
-    //ei_printf_force("Generated signal \n");
 
     EI_IMPULSE_ERROR res = run_classifier(&signal, &result, false);
-
-    //ei_printf_force("run_classifier returned: %d\n", res);
-
-    //ei_printf_force("Begin output\n");
-
-    // print the predictions
-    //ei_printf_force("[");
-    //for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
-        //ei_printf_force_float(result.classification[ix].value);
-
-        //if (EI_CLASSIFIER_HAS_ANOMALY == 1) ei_printf_force(", ");
-        //else if (ix != EI_CLASSIFIER_LABEL_COUNT - 1) ei_printf_force(", ");
-    //}
-
-    //if (EI_CLASSIFIER_HAS_ANOMALY == 1) ei_printf_force_float(result.anomaly);
-
-    //ei_printf_force("]\n");
-
-    //ei_printf_force("End output\n");
 
     // Finally return the probability of having spotted the wanted sound to the AudioMoth system
 
